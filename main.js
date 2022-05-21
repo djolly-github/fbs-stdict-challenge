00f0153b129e9e2fc48640c329c896e27582b5e3
Lightling <contactlightling@gmail.com>
2022-05-21 16:36:17 -0400

    test retrieval ok

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 const init = () => {
-  document.querySelector('#app').innerHTML = 'App is working!';
+  const uriDictionary = 'dictionary.txt'
+  const uriStory = 'story.txt'
+
+  const elementDictionary = document.querySelector('#dictionary');
+  const elementStory = document.querySelector('#story');
+
+  Promise.all([uriDictionary, uriStory].map((uri) => fetch(uri)))
+    .then((responses) => {
+      console.log(responses.map((response) => response.text()));
+    });
 };
 
 window.onload = init;

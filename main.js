3df1348a6f63d1c62f6bf42272ddd6a9d0bb9927
Lightling <contactlightling@gmail.com>
2022-05-21 17:35:38 -0400

    improve parsing

--- main.js
+++ main.js
@@ -13,6 +13,19 @@ const fetchParams = {
 
 };
 
+const regex = {
+  whitespace: new RegExp(/\n|\s/gm),
+  punctuation: new RegExp(/[^(\w|\d|^\'|\u2019)]/gm),
+};
+
+const splitAndTrim = (str, regex) => {
+  return str.trim().split(regex).filter((string) => string !== '')
+};
+
+const filterWordsNotPresent = (words) => {
+  console.log(words);
+};
+
 const resolveBodyElements = () => {
   elements.dictionary = document.querySelector('#dictionary ul');
   elements.story = document.querySelector('#story p');
@@ -27,17 +40,19 @@ const getTextData = async () => {
 
 const parseTextData = async (responses) => {
   const parsed = await Promise.all(responses.map((response) => response.text()));
-  const splitText = parsed.map((text) => text.split('\n').filter((string) => string !== ''));
-  return Promise.resolve(splitText);
+  return parsed;
 }
 
 const resolveParsedTextData = async (data) => {
-  data[0].forEach((string) => {
+  const dictionaryWords = splitAndTrim(data[0], regex.whitespace);
+  dictionaryWords.forEach((string) => {
     const listElement = document.createElement('li');
     listElement.innerHTML = string;
     elements.dictionary.appendChild(listElement);
   });
   elements.story.innerHTML = data[1];
+  const storyWords = splitAndTrim(data[1], new RegExp(regex.whitespace.source + '|' + regex.punctuation.source))
+  filterWordsNotPresent(storyWords);
   return Promise.resolve(data);
 }
 

04051328075c8d8559ef47d432954f06a9624914
Lightling <contactlightling@gmail.com>
2022-05-21 17:02:13 -0400

    text parsing

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
+const requestURIs = {
+  dictionary: 'dictionary.txt',
+  story: 'story.txt',
+};
+
+const elements = {
+  dictionary: null,
+  story: null,
+  words: null
+}
+
+const fetchParams = {
+
+};
+
+const resolveBodyElements = () => {
+  elements.dictionary = document.querySelector('#dictionary');
+  elements.story = document.querySelector('#story');
+  elements.words = document.querySelector('#words');
+};
+
+const getTextData = async () => {
+  const requests = Object.values(requestURIs).map((uri) => fetch(uri, fetchParams));
+  const responses = await Promise.all(requests);
+  return responses;
+}
+
+const parseTextData = async (responses) => {
+  const parsed = await Promise.all(responses.map((response) => response.text()));
+  const splitText = parsed.map((text) => text.split('\n').filter((string) => string !== ''));
+  return Promise.resolve(splitText);
+}
+
+const resolveParsedTextData = async (data) => {
+  console.log(data)
+  return Promise.resolve(data);
+}
+
 const init = () => {
-  const uriDictionary = 'dictionary.txt'
-  const uriStory = 'story.txt'
-
-  const elementDictionary = document.querySelector('#dictionary');
-  const elementStory = document.querySelector('#story');
-
-  Promise.all([uriDictionary, uriStory].map((uri) => fetch(uri)))
-    .then((responses) => {
-      console.log(responses.map((response) => response.text()));
-    });
+  resolveBodyElements();
+  getTextData()
+    .then(parseTextData)
+    .then(resolveParsedTextData)
 };
 
 window.onload = init;
